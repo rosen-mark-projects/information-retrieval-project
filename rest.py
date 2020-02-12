1fce3fe4ff86d5eee7acde2c261cc54f6a442a27
mark95 <mark199@abv.bg>
2020-02-12 21:21:34 +0200

    s=upgrade rest.py

--- rest.py
+++ rest.py
@@ -1,10 +1,24 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 import psycopg2
 
-conn = psycopg2.connect(database="tweet_db", user="postgres", password="postgres", host="127.0.0.1", port="5432")
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), '.env'), override=True)
+
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASS = os.environ.get('POSTGRES_PASS')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
+POSTGRES_DB = os.environ.get('POSTGRES_DB', 'tweet_db')
+conn = psycopg2.connect(database=POSTGRES_DB, user=POSTGRES_USER,
+                        password=POSTGRES_PASS, host=POSTGRES_HOST)
 
 
 app = Flask(__name__)
+
 
 @app.route('/tweets', methods=['GET'])
 def get_tweets():
@@ -16,12 +30,15 @@ def get_tweets():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 def get_tweets_from_db(keyword):
     cur = conn.cursor()
-    cur.execute(f'SELECT text, keyword, link FROM disaster_tweets WHERE keyword={keyword}')
+    cur.execute(
+        f"SELECT text, keyword, url FROM disaster_tweets WHERE keyword='{keyword}'")
     data = cur.fetchall()
     return data
     # TODO
+
 
 def convert_to_dicts(tweets):
     dicts = []
@@ -31,8 +48,9 @@ def convert_to_dicts(tweets):
         dict['keyword'] = tweet[2]
         dict['link'] = tweet[3]
         dict['target'] = 0
+        dicts.append(dict)
 
-    dicts.append(dict)
     return dicts
+
 
 app.run()
