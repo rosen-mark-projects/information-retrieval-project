d2eb9a17712f85ddd1978b2b844358893b66a3cc
Rosen Krumov <rosenkrumov95@gmail.com>
2020-02-13 15:00:15 +0200

    update web visualization of tweets

--- webservice/client.js
+++ webservice/client.js
@@ -2,18 +2,38 @@ $(document).ready(function() {
     $('#tweetSubmit').click(searchTweets);
 
     function searchTweets() {
-        var text = $.trim($('#tweetText').val());
+        var keyword = $.trim($('#tweetText').val());
         $.ajax({
             url: 'http://localhost:5000/tweets',
             type: 'GET',
             data: {
-                text: text
+                text: keyword
             },
             success: function(data){
                 console.log(data)
+                $('#tweetsContainer').empty()
+                var htmlRender = `  <div class="row">
+                                        <div class="col-sm-12 text-center">
+                                            <br>
+                                            <p>You searched tweets, related to <b><i>${keyword}</i></b></p>
+                                        </div>
+                                    </div>`;
+                data.forEach(element => {
+                    htmlRender += `<div class="row">
+                                        <div class="col"></div>
+                                        <div class="col-12">
+                                            <hr>
+                                            <p>${element.text}</p>
+                                            <a href="${element.url}">${element.url}</a>
+                                            <p>Truth: ${element.target === 1 ? 'Yes' : 'No'}</p>
+                                        </div>
+                                        <div class="col"></div>
+                                    </div>`
+                });
+                $('#tweetsContainer').append(htmlRender);
             },
             error: function(data){
-
+                console.log(data)
             }
         });
     }
